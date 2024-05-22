a777c1656b2310db06b22b8cd9553de43494f4ab
jingkai27 <jingkai.t27@gmail.com>
2024-05-22 11:54:21 +0800

    day1: starting phase

---
+++ landing_page/script.js
@@ -0,0 +1,8 @@
+function downloadApp() {
+    alert("Download link coming soon!");
+}
+
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Thank you for your message! We will get back to you soon.");
+});
