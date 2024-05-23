f628631ca7f45875ad78b99d034c675ade8bbf24
jingkai27 <jingkai.t27@gmail.com>
2024-05-23 12:56:39 +0800

    day2: mobile optimisation

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ function downloadApp() {
     alert("Download link coming soon!");
 }
 
+function toggleMenu() {
+    var navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show');
+}
+
+
 document.getElementById("contactForm").addEventListener("submit", function (event) {
     event.preventDefault();
     alert("Thank you for your message! We will get back to you soon.");
