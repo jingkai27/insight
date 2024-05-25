480c76b15aec80f7b0ed027670388419922bd710
jingkai27 <jingkai.t27@gmail.com>
2024-05-25 11:05:48 +0800

    update

--- script.js
+++ script.js
@@ -2,13 +2,57 @@ function downloadApp() {
     document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
 }
 
+function showModal() {
+    var modal = document.getElementById("myModal");
+    var span = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "block";
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
 function toggleMenu() {
     var navLinks = document.getElementById('nav-links');
     navLinks.classList.toggle('show');
 }
 
-
-document.getElementById("contactForm").addEventListener("submit", function (event) {
+document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert("Thank you for your message! We will get back to you soon.");
+
+    var form = event.target;
+    var data = new FormData(form);
+
+    fetch(form.action, {
+        method: 'POST',
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            form.reset();
+            showModal();
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    alert(data["errors"].map(error => error["message"]).join(", "));
+                } else {
+                    alert('Form submission failed.');
+                }
+            });
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Form submission failed.');
+    });
 });
